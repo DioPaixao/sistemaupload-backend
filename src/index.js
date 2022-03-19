6d16ddb9e119adbccb1e5fe45d0dbac380499b2f
Diogo Paixão <69406859+DioPaixao@users.noreply.github.com>
2022-03-19 19:01:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,4 +21,4 @@ app.use('/file', express.static(path.resolve(__dirname, '..','temp','uploads')))
 
 app.use(require('./routes.js'))
 
-app.listen(process.env.PORT || 3000)
+app.listen(Process.env.PORT || 3000)
