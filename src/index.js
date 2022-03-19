e9f481ff486b8dd1e42cf1b47a6ad14c8744941d
Diogo Paixão <69406859+DioPaixao@users.noreply.github.com>
2022-03-19 18:47:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 require("dotenv").config()
 
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const path = require('path')
-const cors = require('cors')
 
 const app = express()
 
